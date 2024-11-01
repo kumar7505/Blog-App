a46d41d5ed25ac345e91d261c21656c404a458e8
kumar7505 <kumarpers7@gmail.com>
2024-11-01 21:28:36 +0530

    fix: render components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,9 @@ import { useState } from 'react';
 import Header from './components/header.jsx';
 import Post from './components/post.jsx';
 import {Routes, Route} from 'react-router-dom';
-import Layout from './components/layout.jsx'
+import Layout from './components/layout.jsx';
+import IndexPages from './components/pages/indexpage.jsx';
+import Loginpage from './components/pages/loginpage.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,8 +12,8 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={ <Post /> }/>
-        <Route path={'/login'} element={ <p>login page</p> } />
+        <Route index element={ <IndexPages /> }/>
+        <Route path="/login" element={ <Loginpage /> } />
       </Route>
     </Routes>
 
