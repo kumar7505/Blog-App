8bfd0363c2f0c4319cc2eb5eb1f53b2dbe2fc21c
kumar7505 <kumarpers7@gmail.com>
2024-11-02 23:44:51 +0530

    authen1

--- api/index.js
+++ api/index.js
@@ -1,17 +1,24 @@
 const express= require('express');
 const cors= require('cors');
+const mongoose = require('mongoose');
+const User = require('/models/user');
+
+require('dotenv').config();
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+mongoose.connnect(process.env.mongo_url);
+
 app.get('/test', (req, res) => {
     res.json({ message: 'ok'});
 });
 
-app.post('/register', (req, res) => {
+app.post('/register',async (req, res) => {
     const { username, password }= req.body;
-    res.json({ message: {username, password}});
+    const userDoc = await User.create({username, password});
+    res.json(userDoc);
     console.log(98);
 });
 
