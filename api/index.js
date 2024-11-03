a013638c9eae9148a5607c3a1ae2e260a20acef4
kumar7505 <kumarpers7@gmail.com>
2024-11-04 00:02:40 +0530

    feat: login

--- api/index.js
+++ api/index.js
@@ -3,14 +3,17 @@ const cors= require('cors');
 const mongoose = require('mongoose');
 const User = require('./models/user.js');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const salt = bcrypt.genSaltSync(10);
+const secret = '5yvhedsi8ejhrf5ejhbdrei7576edrft';
 require('dotenv').config();
 const app = express();
 
+const REACT_PORT = process.env.REACT_PORT
 const MONGO_URL = process.env.MONGO_URL;
 
-app.use(cors());
+app.use(cors({credentials: true, origin: `http://localhost:5173`}));
 app.use(express.json());
 
 mongoose.connect(MONGO_URL)
@@ -19,17 +22,27 @@ mongoose.connect(MONGO_URL)
     })
     .catch(err => console.error('MongoDB connection error:', err));
 
-
-app.get('/test', (req, res) => {
-    res.json({ message: 'ok' });
-});
-
 app.post('/register',async (req, res) => {
     const { username, password }= req.body;
-        const userDoc = await User.create({
-            username, 
-            password: bcrypt.hashSync(password, salt)});
-        res.json(userDoc);
+    const userDoc = await User.create({
+        username, 
+        password: bcrypt.hashSync(password, salt)});
+    res.json(userDoc);
+});
+
+app.post('/login', async (req, res) => {
+    const { username, password }= req.body;
+    const userDoc = await User.findOne({ username });
+    const pass0k = bcrypt.compareSync(password, userDoc.password);
+    console.log('kumar');
+    if(pass0k){
+        jwt.sign({username, id: userDoc._id}, secret, {}, (err, token) => {
+            if(err) throw err;
+            res.cookie('token', token).json('ok');
+        })
+    } else {
+        res.status(400).json('Wrong Credintials');
+    }
 });
 
 app.listen(8000);
